723eff9fdbaaadbe74d961a4a8342181f6dcc8ce
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2024-11-09 20:29:50 -0600

    POST, GET set-up

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.send("hello from the server!")
+app.get("/user", (req, res) => {
+    res.send({firstname: "Sai Dinesh", lastname: "Kopparthi", age: 25})
+});
+
+app.post("/uset", (req, res) => {
+    res.send("The user details has been updated successfully!");
+});
+
+app.use("/", (req, res) => {
+    res.send("You have reached the home page. \nHOLA!!!");
 })
 
 app.listen(3000, ()=>console.log("Listening on port 3000"));
