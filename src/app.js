7ab83acdb0227f4cdc0fe98a4f838ef4700bc1c9
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2024-11-10 21:51:42 -0600

    Why multi middleware function

--- src/app.js
+++ src/app.js
@@ -2,19 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.use("/user", 
-    (req, res, next) => {
-    console.log("Handling the route user");
-    //If we use this next() function the next route handler will be invoked.
-    //This next() function is given by Express.js
-    res.send("Handles here first");
-    next(); 
-    },
-    (req, res) => {
-        console.log("Handling the route user - two");
-        res.send("Handled");
-    } //In the first route handler there is no response being sent. 
-    //So if we hit this API, it will run for infinitely, even though we have a second route handler with res.send();
-)
+app.use("/admin", (req, res, next) => {
+    console.log("Admin authorization is being checked");
+
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    
+    if(!isAdminAuthorized){
+        res.status(401).send("The admin doesn't have proper access");
+    } else {
+        next();
+    }
+});
+
+app.get("/user", (req, res) => {
+    res.send("User data has been sent successfully");
+})
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All the users data is sent.")
+})
+
+app.get("/admin/updateData", (req, res) => {
+    res.send("Data has been updated successfully.")
+})
 
 app.listen(3000, () => console.log("Listening on port 3000"));
