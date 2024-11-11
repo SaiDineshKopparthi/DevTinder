831de391e65832495f404145c61bea801fb006b8
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2024-11-10 23:15:11 -0600

    Error Handling

--- src/app.js
+++ src/app.js
@@ -5,20 +5,19 @@ const app = express();
 
 app.use("/admin", adminAuth);
 
-app.get("/user/login", (req, res) => {
-    res.send("User login successful");
-})
+app.get("/getUserData", (req, res) => {
+    try{
+        throw new Error("Gotcha!");
+    } catch (err) {
+        res.status(500).send("Some Error: Contact Support Team.")
+    }
+    
+});
 
-app.get("/user/data", userAuth, (req, res) => {
-    res.send("User data has been sent successfully");
-})
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All the users data is sent.")
-})
-
-app.get("/admin/updateData", (req, res) => {
-    res.send("Data has been updated successfully.")
-})
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send(`Something went wrong - ${err}`);
+    }
+});
 
 app.listen(3000, () => console.log("Listening on port 3000"));
