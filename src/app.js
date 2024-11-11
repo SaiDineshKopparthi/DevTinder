e3fe0d7ad669e4fccf2f4fb3a40d25eda6660270
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2024-11-10 19:27:00 -0600

    Route Handling Use Cases

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({firstname: "Sai Dinesh", lastname: "Kopparthi", age: 25})
-});
+app.use("/user", 
+    (req, res, next) => {
+    console.log("Handling the route user");
+    //If we use this next() function the next route handler will be invoked.
+    //This next() function is given by Express.js
+    res.send("Handles here first");
+    next(); 
+    },
+    (req, res) => {
+        console.log("Handling the route user - two");
+        res.send("Handled");
+    } //In the first route handler there is no response being sent. 
+    //So if we hit this API, it will run for infinitely, even though we have a second route handler with res.send();
+)
 
-app.post("/uset", (req, res) => {
-    res.send("The user details has been updated successfully!");
-});
-
-app.use("/", (req, res) => {
-    res.send("You have reached the home page. \nHOLA!!!");
-})
-
-app.listen(3000, ()=>console.log("Listening on port 3000"));
+app.listen(3000, () => console.log("Listening on port 3000"));
