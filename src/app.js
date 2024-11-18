d2efaeef379ffbbb5e0d15377f0072414995cca8
Sai Dinesh Kopparthi <kopparthi.dinesh42224@gmail.com>
2024-11-17 18:43:44 -0600

    Updated user API's with CRUD operations

--- src/app.js
+++ src/app.js
@@ -4,10 +4,9 @@ const User = require("./Models/user")
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-
     //Create a new "user" document using "User", model
     const user = new User(req.body);
 
@@ -17,7 +16,63 @@ app.post("/signup", async (req, res) => {
     } catch (error) {
         res.status(400).send("Error while creating the user: " + error.message);
     }
-})
+});
+
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        if(users.length === 0) {
+            res.send("No users available")
+        } else {
+            res.send(users);
+        }
+    } catch (error) {
+        
+    }  
+});
+
+app.get("/user", async (req, res) => {
+    const emailID = req.body.emailID;
+
+    try {
+        const user = await User.findOne({emailID: emailID});
+
+        if(!user) {
+            res.send("No user found with the given email")
+        }else {
+            res.send(user)
+        }
+    }catch(error) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+app.patch("/user", async (req, res) => {
+    //const id = req.body.userID;
+    const emailID = req.body.emailID;
+    const data = req.body;
+
+    try{
+        // await User.findByIdAndUpdate({_id : id}, data);
+        await User.updateOne({emailID: emailID}, data);
+        res.send("User data updated successfully")
+    }catch(error) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const userID = req.body.userID;
+    console.log(userID)
+
+    try {
+        await User.findByIdAndDelete({_id : userID});
+        res.send("User deleted from the collection!");
+
+    }catch(error) {
+        res.status(400).send("Something went wrong");
+    }
+});
 
 //Connecting to Database and Making the server listen for requests
 connectDB().then(() => {
